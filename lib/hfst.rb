5c70ceb860543c7433bc0d608c6626bf04dbff56
Tazro Inutano Ohta <inutano@gmail.com>
2016-11-30 00:18:16 +0900

    redefine module methods

--- lib/hfst.rb
+++ lib/hfst.rb
@@ -4,13 +4,15 @@ require "hfst/ebirnaseqapi"
 require "hfst/sra"
 
 module HFST
-  def get_chipseq(antigen, cell_line_name, experiments_list_fpath, base)
-    ChIPAtlas.new(antigen, cell_line_name, experiments_list_fpath).bedfiles(base)
-  end
+  class << self
+    def get_chipseq(antigen, cell_line_name, experiments_list_fpath, base)
+      ChIPAtlas.new(antigen, cell_line_name, experiments_list_fpath).bedfiles(base)
+    end
 
-  def get_rnaseq(cell_line_name, sparql_template, run_table)
-    bs_ids = EBIRDF.new(cln).post(sparql_template)
-    runids = bs_ids.map{|bsid| SRA.biosample_to_runid(run_table, bsid) }.flatten.uniq
-    runids.map{|runid| EBIRNAseqAPI::Run.new(runid).bigwig_url }.flatten.uniq
+    def get_rnaseq(cell_line_name, sparql_template, run_table)
+      bs_ids = EBIRDF.new(cln).post(sparql_template)
+      runids = bs_ids.map{|bsid| SRA.biosample_to_runid(run_table, bsid) }.flatten.uniq
+      runids.map{|runid| EBIRNAseqAPI::Run.new(runid).bigwig_url }.flatten.uniq
+    end
   end
 end
