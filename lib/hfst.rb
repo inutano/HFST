6c29144f732d7a2563d005f561e8f31238b2d30e
Tazro Inutano Ohta <inutano@gmail.com>
2016-11-29 23:08:49 +0900

    get files via endpoint and api

--- lib/hfst.rb
+++ lib/hfst.rb
@@ -1,2 +1,16 @@
+require "hfst/chipatlas"
+require "hfst/ebirdf"
+require "hfst/ebirnaseqapi"
+require "hfst/sra"
+
 module HFST
+  def get_chipseq(antigen, cell_line_name, experiments_list_fpath, base)
+    ChIPAtlas.new(antigen, cell_line_name, experiments_list_fpath).bedfiles(base)
+  end
+
+  def get_rnaseq(cell_line_name, sparql_template, run_table)
+    bs_ids = EBIRDF.new(cln).post(sparql_template)
+    runids = bs_ids.map{|bsid| SRA.biosample_to_runid(run_table, bsid) }.flatten.uniq
+    runids.map{|runid| EBIRNAseqAPI::Run.new(runid).bigwig_url }.flatten.uniq
+  end
 end
