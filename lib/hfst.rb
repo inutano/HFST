a87691ce95e65094a3c91cdc4e0a4047a2e8112e
Tazro Inutano Ohta <inutano@gmail.com>
2016-11-30 00:53:27 +0900

    fixed internal server error over sparql query

--- lib/hfst.rb
+++ lib/hfst.rb
@@ -10,7 +10,7 @@ module HFST
     end
 
     def get_rnaseq(cell_line_name, sparql_template, run_table)
-      bs_ids = EBIRDF.new(cln).post(sparql_template)
+      bs_ids = EBIRDF.new(cell_line_name).post(sparql_template)
       runids = bs_ids.map{|bsid| SRA.biosample_to_runid(run_table, bsid) }.flatten.uniq
       runids.map{|runid| EBIRNAseqAPI::Run.new(runid).bigwig_url }.flatten.uniq
     end
--- lib/hfst/ebirdf.rb
+++ lib/hfst/ebirdf.rb
@@ -7,7 +7,7 @@ module HFST
     end
 
     def build_query(template)
-      query = open(template).read.sub(/CELL_LINE_NAME/,@cln)
+      query = open(template).read.sub(/CELL_LINE_NAME/,@cln.downcase)
       URI.encode(query)
     end
 
@@ -20,7 +20,7 @@ module HFST
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      req = Net::HTTP::Post.new(uri.path)
+      req = Net::HTTP::Post.new(endpoint_url.path)
       req.set_form_data({
         "query" => build_query(template),
         "format" => "json",
