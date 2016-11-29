b38c9637ad5bb4e3e8a96280a86e2aae401e550b
Tazro Inutano Ohta <inutano@gmail.com>
2016-11-29 23:04:55 +0900

    get chipseq data id from experiments table

---
+++ lib/hfst/chipatlas.rb
@@ -0,0 +1,20 @@
+module HFST
+  class ChIPAtlas
+    class << self
+      def load_experiments(fpath)
+        open(fpath).readlines.map{|line| line.chomp.split("\t") }
+      end
+    end
+
+    def initialize(antigen, cell_line, experiments_list_fpath)
+      @ag = antigen
+      @cl = cell_line
+      @list = experiments_list_fpath
+    end
+
+    def bedfiles(base_dir)
+      expids = `cat #{@list} | awk -F '$4 == #{@ag} && $6 == #{@cl} { print $1 }'`.split("\n")
+      expids.map{|id| File.join(base_dir, id.sub(/...$/,""), id + ".bed") }
+    end
+  end
+end
